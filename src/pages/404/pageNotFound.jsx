3ac2afca4ff92504aa875770d2ec4472ea1ed2a2
Ankit Pandey <ankitpandey.272003@gmail.com>
2023-08-14 16:25:54 +0530

    Created PageNotFound component

--- src/pages/404/pageNotFound.jsx
+++ src/pages/404/pageNotFound.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
-import './style.scss'
-const pageNotFound = () => {
-  return (
-    <div>
-      pag not found
-    </div>
-  )
-}
+import React from "react";
 
-export default pageNotFound
+import "./style.scss";
+
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
+
+const PageNotFound = () => {
+    return (
+        <div className="pageNotFound">
+            <ContentWrapper>
+                <span className="bigText">404</span>
+                <span className="smallText">Page not found!</span>
+            </ContentWrapper>
+        </div>
+    );
+};
+
+export default PageNotFound;
+
