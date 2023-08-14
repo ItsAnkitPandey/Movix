733d27b9bdbf45ec218d260bafbd8cd8b1076f61
Ankit Pandey <ankitpandey.272003@gmail.com>
2023-08-14 16:50:00 +0530

    api to Api

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from 'react'
+import {  useEffect } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { fetchDataFromApi } from './Utils/api'
+import { fetchDataFromApi } from './Utils/Api'
 
 import { useSelector, useDispatch } from 'react-redux'
 import { getApiConfiguration, getGenres } from './Store/homeSlice'
