ba9d91e61543a9532d77331af4ea99113e5e7bf1
Ankit Pandey <ankitpandey.272003@gmail.com>
2023-08-14 10:19:42 +0530

    create simiilar and recommendation and some change

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -13,7 +13,7 @@ import PosterFallback from "../../assets/no-poster.png";
 import "./style.scss";
 import Genres from '../genres/Genres';
 
-const Carousel = ({ data, loading, endpoint }) => {
+const Carousel = ({ data, loading, endpoint, title }) => {
     const carouselContainer = useRef();
     const { url } = useSelector((state) => state.home);
     const navigate = useNavigate();
@@ -43,6 +43,7 @@ const Carousel = ({ data, loading, endpoint }) => {
     return (
         <div className='carousel'>
             <ContentWrapper>
+            {title && <div className='carouselTitle'>{title}</div>}
                 <BsFillArrowLeftCircleFill className='carouselLeftNav arrow' onClick={() => navigation("left")} />
                 <BsFillArrowRightCircleFill className='carouselRightNav arrow' onClick={() => navigation("right")} />
                 {!loading ? (
--- src/pages/searchResult/SearchResult.jsx
+++ src/pages/searchResult/SearchResult.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
+import InfiniteScroll from 'react-infinite-scroll-component'
+import { useParams } from 'react-router-dom'
+import ContentWrapper from '../../components/contentWrapper/ContentWrapper'
+import { fetchDataFromApi } from '../../utils/api'
 import './style.scss'
+import noResult from '../../assets/no-results.png'
+
 const SearchResult = () => {
+  const [data, setData] = useState(null);
+  const [pageNum, setPageNum] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const { query } = useParams();
+  
   return (
     <div>
       SearchResult
